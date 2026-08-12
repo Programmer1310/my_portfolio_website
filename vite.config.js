b661189508b6df5390193df32575cb240ce41b33
Programmer1310 <amruthashyam13@gmail.com>
2026-08-12 13:03:36 -0400

    Change build output directory to docs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/my_portfolio_website/', // <--- Add your repository name here wrapped in slashes
+  base: '/my_portfolio_website/',
+  build: {
+    outDir: 'docs', // <--- Outputs build to /docs instead of /dist
+  },
 })
